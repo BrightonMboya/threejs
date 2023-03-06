4e53d50e7322f73af9635167467e26a2307aee66
BrightonMboya <b.mboya@alustudent.com>
2023-03-06 22:52:15 +0200

    learned messing up with the camera

--- src/components/basic-scene/index.ts
+++ src/components/basic-scene/index.ts
@@ -1,5 +1,19 @@
 import * as THREE from 'three';
 import gsap from 'gsap';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+
+/* 
+stuff for moving the camera with a mouse
+*/
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5; // so that we have a range of -0.5 to 0.5
+    cursor.y = -(event.clientY / sizes.height - 0.5);
+});
 
 const scene = new THREE.Scene();
 // creating the mesh cube
@@ -14,7 +28,20 @@ const sizes = {
     height: 600
 }
 // creating the camera
+/* 
+1.Field of View [Vertical FOV]
+2.Aspect Ratio
+3.Near
+4.Far
+*/
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+// controls for the OrbitControl cam
+const controls = new OrbitControls(camera, document.querySelector('.webgl')! as HTMLElement);
+controls.enableDamping = true;
+
+
+//u can also use orthographic camera
+// const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0.1, 100);
 camera.position.z = 3;
 scene.add(camera)
 
@@ -49,18 +76,25 @@ document.body.appendChild(renderer.domElement);
 const clock = new THREE.Clock();
 
 //using gsap to do the animations
-gsap.to(cube.position, { duration: 1, delay: 1, x: 2 });
-gsap.to(cube.position, { duration: 1, delay: 2, x: 0 });
+// gsap.to(cube.position, { duration: 1, delay: 1, x: 2 });
+// gsap.to(cube.position, { duration: 1, delay: 2, x: 0 });
 
 function rotateCube() {
     //fixing the fps on diff computers
     const elapsedTime = clock.getElapsedTime();
 
     requestAnimationFrame(rotateCube);
+    // updating the camera with the cursor
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+    // camera.position.y = cursor.y * 5;
+    // camera.lookAt(cube.position);
+
     // cube.rotation.y = elapsedTime;
     // cube.rotation.x = elapsedTime
     // cube.position.y = Math.sin(elapsedTime);
     // cube.position.x = Math.cos(elapsedTime);
+    controls.update();
 
 
     renderer.render(scene, camera);
