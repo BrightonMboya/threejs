4fb030a1f551a30665837c4950076c7be3e5209d
BrightonMboya <b.mboya@alustudent.com>
2023-03-12 19:54:18 +0200

    finished learning Lights

--- src/components/basic-scene/light.ts
+++ src/components/basic-scene/light.ts
@@ -18,19 +18,47 @@ const scene = new THREE.Scene()
 /**
  * Lights
  */
+
+// the first two are low cost, the last two are high cost
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(ambientLight);
+
+const hemispherLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3);
+scene.add(hemispherLight);
+
 
 const directionalLight = new THREE.DirectionalLight(0x00fffc, 0.3);
 directionalLight.position.set(1, 0.25, 0)
 scene.add(directionalLight);
 
-const hemispherLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3);
-scene.add(hemispherLight);
 
 const pointLight = new THREE.PointLight(0xff9000, 0.5);
+pointLight.position.set(1, -0.5, 1)
 scene.add(pointLight);
 
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 1);
+rectAreaLight.position.set(-1.5, 0, 1.5);
+rectAreaLight.lookAt(new THREE.Vector3())
+scene.add(rectAreaLight);
+
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1);
+spotLight.position.set(0, 2, 3)
+spotLight.target.position.x = -0.75
+scene.add(spotLight);
+scene.add(spotLight.target);
+
+/* Helpers */
+const hemispherLightHelper = new THREE.HemisphereLightHelper(hemispherLight, 0.2);
+scene.add(hemispherLightHelper);
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 0.2);
+scene.add(directionalLightHelper);
+
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2);
+scene.add(pointLightHelper);
+
+const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+scene.add(spotLightHelper);
 /**
  * Objects
  */
